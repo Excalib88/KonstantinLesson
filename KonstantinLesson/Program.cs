4e8cf8a421c27496cd9a5a69f16c02e9ccd2e221
Damir Mansurov <rave9373@gmail.com>
2021-03-11 00:50:30 +0300

    initial commit

---
+++ KonstantinLesson/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace KonstantinLesson
+{
+	public interface IInterface
+	{
+		string GetName(long id);
+	}
+
+	public class Person : IInterface
+	{
+		public string GetName(long id)
+		{
+			return "Vasya";
+		}
+	}
+
+	public class People : IInterface
+	{
+		public string GetName(long id)
+		{
+			return "Petya";
+		}
+	}
+	
+	public class Employee
+	{
+		private readonly IInterface _person;
+		
+		public Employee(IInterface person)
+		{
+			_person = person;
+		}
+		public string Name { get; set; }
+		public void SetName(int id)
+		{
+			
+			Name = _person.GetName(id);
+		}
+	}
+
+	class Program
+	{
+		public string Name { get; set; }
+		
+		static void Main(string[] args)
+		{
+			var person = new Employee(new People()); 
+			person.SetName(1);
+			person.SetName(1);
+			Console.WriteLine(person.Name);
+		}
+	}
+}
